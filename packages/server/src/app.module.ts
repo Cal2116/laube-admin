c55df92bd332ae47967cc29b34125ad0a6461288
chenyucong <1803861304@qq.com>
2025-04-13 00:06:18 +0800

    fix: 注册auth模块

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,11 +2,19 @@ import { ConfigModule } from '@core/config/config.module'
 import { LoggerModule } from '@core/logger/logger.module'
 import { PrismaModule } from '@core/prisma/prisma.module'
 import { RedisModule } from '@core/redis/redis.module'
+import { AuthModule } from '@modules/auth/auth.module'
 import { SystemModule } from '@modules/system/system.module'
 import { Module } from '@nestjs/common'
 
 @Module({
-  imports: [ConfigModule, LoggerModule, RedisModule, PrismaModule.forRootAsync(), SystemModule],
+  imports: [
+    ConfigModule,
+    LoggerModule,
+    RedisModule,
+    PrismaModule.forRootAsync(),
+    SystemModule,
+    AuthModule,
+  ],
   controllers: [],
   providers: [],
 })
