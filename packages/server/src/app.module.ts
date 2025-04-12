7d1e4273f33e164a3824e7e4c5ab4952a7a5208c
chenyucong <1803861304@qq.com>
2025-04-12 16:58:48 +0800

    feat: Nest 统一返回值结构

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 
 import { ConfigModule } from './core/config/config.module'
 import { LoggerModule } from './core/logger/logger.module'
+import { PrismaModule } from './core/prisma/prisma.module'
 import { RedisModule } from './core/redis/redis.module'
 
 @Module({
-  imports: [ConfigModule, LoggerModule, RedisModule],
+  imports: [ConfigModule, LoggerModule, RedisModule, PrismaModule.forRootAsync()],
   controllers: [],
   providers: [],
 })
