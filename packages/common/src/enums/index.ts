587f9dbfeed025f66ff63255f6d270f0ebf39177
chenyucong <1803861304@qq.com>
2025-04-13 12:33:38 +0800

    feat: 通用枚举与登录接口请求响应类型定义

---
+++ packages/common/src/enums/index.ts
@@ -0,0 +1,14 @@
+export enum ResultCode {
+  SUCCESS = 200,
+  ERROR = -1,
+}
+
+export enum ResultMessage {
+  SUCCESS = '操作成功',
+  ERROR = '操作失败',
+}
+
+export enum EnableStatus {
+  ENABLE = 1,
+  DISABLE = 0,
+}
