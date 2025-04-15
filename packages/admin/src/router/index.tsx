a934f323590abca0e9bc1ffe516676f4f64ebf01
chenyucong <1803861304@qq.com>
2025-04-15 13:23:39 +0800

    feat: 路由守卫

--- packages/admin/src/router/index.tsx
+++ packages/admin/src/router/index.tsx
@@ -1,15 +1,47 @@
+import { StorageKey } from '@laube-admin/common'
+import { message } from 'antd'
 import { lazy } from 'react'
-import { createBrowserRouter, Navigate } from 'react-router-dom'
+import { createBrowserRouter, Navigate, redirect } from 'react-router-dom'
 
+import { getStorageValue } from '@/utils'
 import Login from '@/views/Login'
+
+const checkLogin = () => {
+  const accessToken = getStorageValue(StorageKey.ACCESS_TOKEN)
+
+  return !!accessToken
+}
+
+const loginLoader = () => {
+  const isLogin = checkLogin()
+
+  if (isLogin) {
+    return redirect('/')
+  }
+
+  return null
+}
+
+const protectedLoader = () => {
+  const isLogin = checkLogin()
+
+  if (!isLogin) {
+    message.error('请先完成登录')
+    return redirect('/login')
+  }
+
+  return null
+}
 
 const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+    loader: loginLoader,
   },
   {
     Component: lazy(() => import('@/views/Home')),
+    loader: protectedLoader,
     children: [
       {
         path: '/',
